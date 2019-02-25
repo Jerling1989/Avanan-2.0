ac7b3c262e02ec68b1e437d246d778654a71b13d
Jacob <jerling1989@gmail.com>
2019-02-25 17:16:08 -0500

    change offset top

--- assets/js/custom/tabs.js
+++ assets/js/custom/tabs.js
@@ -25,7 +25,7 @@ $(document).ready(() => {
 
   if (document.location.hash === '#board/' || document.location.hash === '#team/') {
     $('html, body').animate({
-      scrollTop: $('.test').offset().top+85
+      scrollTop: $('.tab-content').offset().top-150
     }, 'slow');
   }
 
